e820a48707ddacdf2d23b636a9d5578ac54f16c3
texasnathan01 <texasnathan01@tamu.edu>
2022-01-29 21:05:32 -0600

    Installed packages

--- morssey/src/Pages/Home/Home.jsx
+++ morssey/src/Pages/Home/Home.jsx
@@ -1,9 +1,43 @@
 import './Home.scss';
 
+
+
 const HomePage = () => {
-   return (  
-      <div className="home">
-         <h2>Home Page</h2>
+   var timeDash = 200;
+   var timeChar = 350;
+   var timeWord = 1000;
+   var timeMouseDown;
+   var timeMouseUp;
+   var timeBetweenClicks;
+
+   var output = "Morse Code Output: ";
+
+   function mouseDown(){
+      var date = new Date();
+      timeMouseDown = date.getTime();
+      timeBetweenClicks = timeMouseDown - timeMouseUp;
+      if(timeBetweenClicks > timeWord){
+         output = output + " ";   
+      }
+   }
+
+   function mouseUp(){
+      var date = new Date();
+      timeMouseUp = date.getTime();
+      var holdTime = timeMouseUp - timeMouseDown;
+
+      if(holdTime < timeDash){
+         output = output + ".";   
+      }
+      else{
+         output = output + "-";
+      }
+   }
+
+   return (
+      <div>
+         <h2> {output} </h2>
+         <button onMouseDown={mouseDown()} onMouseUp={mouseUp()}>Click me</button>
       </div>
    );
 }
